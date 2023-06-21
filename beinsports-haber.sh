059653df974c973edfb2546493f7696efbf304cd
nontonyuk <sudiana111@gmail.com>
2023-06-21 16:07:23 +0700

    Update beinsports-haber.sh

--- beinsports-haber.sh
+++ beinsports-haber.sh
@@ -7,5 +7,7 @@ python3 -m pip install requests
 cd $(dirname $0)/scripts/
 
 python3 beinsports-haber.py > ../beinsports-haber.m3u8
+python3 BernamaTV.py > ../BernamaTV.m3u8
+python3 supercross.py > ../supercrossTV.m3u8
 
 echo m3u8 grabbed
